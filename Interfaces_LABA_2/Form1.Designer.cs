a8022612b4aec692fb7b14e83580dbfb595e03fe
Дмитрий Саидбаталов <ruslanovitch.dmitrij@yandex.ru>
2022-10-09 15:27:46 +0700

    Добавьте файлы проекта.

---
+++ Interfaces_LABA_2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Interfaces_LABA_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(819, 41);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 337);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(1039, 386);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(100, 28);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(356, 41);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueMember = "X";
+            series1.YValueMembers = "Y";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(456, 369);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Draw as lines",
+            "Draw as spline",
+            "Draw as point"});
+            this.comboBox1.Location = new System.Drawing.Point(819, 386);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1158, 28);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 41);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(336, 378);
+            this.checkedListBox1.TabIndex = 9;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.SelectedValueChanged += new System.EventHandler(this.checkedListBox1_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 453);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Interfaces_LABA_2/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace Interfaces_LABA_2
+{
+    public partial class Form1 : Form
+    {
+        public List<Files> Files { get; set; }
+        public Form1()
+        {
+            Files = new List<Files>();
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            chart1.Series.Clear();
+        }
+
+        private void Data_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            chart1.Series[checkedListBox1.SelectedIndex].Points.DataBind(Files[checkedListBox1.SelectedIndex].Data.List, "X", "Y", "");
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+            Files[checkedListBox1.SelectedIndex].Data.Add(new Data() { X = 0, Y = 0 });
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (int i in checkedListBox1.SelectedIndices)
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                            break;
+                        }
+                    case 1:
+                        {
+                            chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                            break;
+                        }
+                    case 2:
+                        {
+                            chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+                            break;
+                        }
+                }
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            openFileDialog.ShowDialog();
+
+            var s = File.ReadAllLines(openFileDialog.FileName);
+
+            Random r = new Random();
+            Files file = new Files();
+            file.Data = new BindingSource();
+            file.FileName = openFileDialog.FileName;
+            file.Color = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+
+            foreach (var str in s)
+            {
+                var coords = str.Split('\t');
+                file.Data.Add(new Data() { X = double.Parse(coords[0]), Y = double.Parse(coords[1]) });
+            }
+            file.Data.ListChanged += Data_ListChanged;
+            Files.Add(file);
+
+            Series series = new Series();
+            series.LegendText = file.FileName;
+            series.Color = file.Color;
+            series.Points.DataBind(file.Data.List, "X", "Y", "");
+            series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series.Add(series);
+
+            checkedListBox1.Items.Add(file.FileName, true);
+            checkedListBox1.SelectedIndex = checkedListBox1.Items.Count - 1;
+            dataGridView1.DataSource = file.Data;
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            saveFileDialog.ShowDialog();
+            
+            var file = new StreamWriter(saveFileDialog.FileName);
+            foreach (Data point in Files[checkedListBox1.SelectedIndex].Data)
+            {
+                file.WriteLine($"{point.X}\t{point.Y}");
+            }
+            file.Close();      
+        }
+
+        private void checkedListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Files[checkedListBox1.SelectedIndex].Data;
+            switch (chart1.Series[checkedListBox1.SelectedIndex].ChartType)
+            {
+                case System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line:
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        break;
+                    }
+                case System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline:
+                    {
+                        comboBox1.SelectedIndex = 1;
+                        break;
+                    }
+                case System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point:
+                    {
+                        comboBox1.SelectedIndex = 2;
+                        break;
+                    }
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            chart1.Series[e.Index].Enabled = e.NewValue == CheckState.Checked;
+        }
+    }
+
+    public class Data
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    public class Files
+    {
+        public string FileName { get; set; }
+        public Color Color { get; set; }
+        public BindingSource Data { get; set; }
+    }
+}
